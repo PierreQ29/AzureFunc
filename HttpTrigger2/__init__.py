430c151adef8dd318ca83e3b32f3e2b06c03efdf
PierreQ29 <149312362+PierreQ29@users.noreply.github.com>
2024-08-20 15:26:40 +0200

    Update __init__.py

--- HttpTrigger2/__init__.py
+++ HttpTrigger2/__init__.py
@@ -8,19 +8,18 @@ import os
 # Variable globale pour stocker les IDs utilisateurs
 user_ids = []
 
-# Charger les IDs utilisateurs une seule fois lors de l'initialisation du module
-def load_user_ids(connection_string, container_name, file_name):
-    global user_ids
+# Charger un fichier CSV depuis Azure Blob Storage
+def load_csv_file(connection_string, container_name, file_name):
     blob_client = BlobClient.from_connection_string(connection_string, container_name, file_name)
     download_stream = blob_client.download_blob()
     csv_content = download_stream.content_as_text()
     df = pd.read_csv(StringIO(csv_content))
-    user_ids = df['user_id'].tolist()
+    return df
 
 # Initialiser les IDs utilisateurs lors du chargement du module
 connection_string = os.getenv("AZURE_STORAGE_CONNECTION_STRING")
 container_name = "data"
-load_user_ids(connection_string, container_name, "user_id.csv")
+load_csv_file(connection_string, container_name, "clicks_df.csv")
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
