d3d0a6c8ff110fb0fd525febb1be2aff0618240d
PierreQ29 <149312362+PierreQ29@users.noreply.github.com>
2024-08-20 16:53:54 +0200

    Update __init__.py

--- HttpTrigger2/__init__.py
+++ HttpTrigger2/__init__.py
@@ -2,8 +2,9 @@ import logging
 import azure.functions as func
 import pandas as pd
 from azure.storage.blob import BlobClient
-from io import StringIO
+from io import StringIO, BytesIO
 import os
+import pickle
 
 # Charger les IDs utilisateurs une seule fois lors de l'initialisation du module
 def load_user_ids(connection_string, container_name, file_name):
@@ -21,20 +22,20 @@ def load_csv_file(connection_string, container_name, file_name):
     df = pd.read_csv(StringIO(csv_content))
     return df
 
-# def load_article_embeddings(connection_string, container_name, file_name):
-#     blob_client = BlobClient.from_connection_string(connection_string, container_name, file_name)
-#     download_stream = blob_client.download_blob()
-#     pickle_content = download_stream.readall()
-#     articles_emb = pd.read_pickle(io.BytesIO(pickle_content))
-#     articles_emb = pd.DataFrame(articles_emb, columns=["embedding_" + str(i) for i in range(articles_emb.shape[1])])
-#     return articles_emb
+def load_article_embeddings(connection_string, container_name, file_name):
+    blob_client = BlobClient.from_connection_string(connection_string, container_name, file_name)
+    download_stream = blob_client.download_blob()
+    pickle_content = download_stream.readall()
+    articles_emb = pd.read_pickle(io.BytesIO(pickle_content))
+    articles_emb = pd.DataFrame(articles_emb, columns=["embedding_" + str(i) for i in range(articles_emb.shape[1])])
+    return articles_emb
 
 # Initialisation des fichiers et modèles
 connection_string = os.getenv("AZURE_STORAGE_CONNECTION_STRING")
 container_name = "data"
 user_ids = load_user_ids(connection_string, container_name, "user_id.csv")
 clicks_df = load_csv_file(connection_string, container_name, "clicks_df.csv")
-# articles_emb = load_article_embeddings(connection_string, container_name, "articles_embeddings.pickle")
+articles_emb = load_article_embeddings(connection_string, container_name, "articles_embeddings.pickle")
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
