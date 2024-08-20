101d21f537ced69fb72ac5a98e350192adf6b62a
PierreQ29 <149312362+PierreQ29@users.noreply.github.com>
2024-08-20 18:36:31 +0200

    Update __init__.py

--- HttpTrigger2/__init__.py
+++ HttpTrigger2/__init__.py
@@ -36,7 +36,7 @@ connection_string = os.getenv("AZURE_STORAGE_CONNECTION_STRING")
 container_name = "data"
 user_ids = load_user_ids(connection_string, container_name, "user_id.csv")
 clicks_df = load_clicks_file(connection_string, container_name, "clicks_df.csv")
-pickle_content = load_article_embeddings(connection_string, container_name, "articles_embeddings.pickle")
+articles_emb = load_article_embeddings(connection_string, container_name, "articles_embeddings.pickle")
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
