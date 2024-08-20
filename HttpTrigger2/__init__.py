a086544c9dca011c515728286fe093d33464f272
PierreQ29 <149312362+PierreQ29@users.noreply.github.com>
2024-08-20 18:49:03 +0200

    Update __init__.py

--- HttpTrigger2/__init__.py
+++ HttpTrigger2/__init__.py
@@ -5,6 +5,8 @@ from azure.storage.blob import BlobClient
 from io import StringIO, BytesIO
 import os
 import pickle
+import tempfile
+from surprise import dump
 
 # Charger les IDs utilisateurs une seule fois lors de l'initialisation du module
 def load_user_ids(connection_string, container_name, file_name):
@@ -13,7 +15,7 @@ def load_user_ids(connection_string, container_name, file_name):
     csv_content = download_stream.content_as_text()
     df = pd.read_csv(StringIO(csv_content))
     return df['user_id'].tolist()
-   
+
 # Charger le fichier clicks depuis Azure Blob Storage
 def load_clicks_file(connection_string, container_name, file_name):
     blob_client = BlobClient.from_connection_string(connection_string, container_name, file_name)
@@ -31,12 +33,23 @@ def load_article_embeddings(connection_string, container_name, file_name):
     articles_emb = pd.DataFrame(articles_emb, columns=["embedding_" + str(i) for i in range(articles_emb.shape[1])])
     return articles_emb
 
+# Charger le modèle depuis Azure Blob Storage
+def load_model(connection_string, container_name, file_name):
+    blob_client = BlobClient.from_connection_string(connection_string, container_name, file_name)
+    download_stream = blob_client.download_blob()
+    with tempfile.NamedTemporaryFile(delete=False) as temp_model_file:
+        temp_model_file.write(download_stream.readall())
+        temp_model_file_path = temp_model_file.name
+    _, model = dump.load(temp_model_file_path)
+    return model
+
 # Initialisation des fichiers et modèles
 connection_string = os.getenv("AZURE_STORAGE_CONNECTION_STRING")
 container_name = "data"
 user_ids = load_user_ids(connection_string, container_name, "user_id.csv")
 clicks_df = load_clicks_file(connection_string, container_name, "clicks_df.csv")
 articles_emb = load_article_embeddings(connection_string, container_name, "articles_embeddings.pickle")
+model = load_model(connection_string, container_name, "model_nmf.pickle")
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
@@ -44,6 +57,7 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info(f"clicks loaded with {len(clicks_df)} rows.")
     logging.info(f"user loaded with {len(user_ids)} rows.")
     logging.info(f"embed loaded with {len(articles_emb)} rows.")
+    logging.info("model_nmf.pickle téléchargé avec succès.")
     
     name = req.params.get('name')
     if not name:
