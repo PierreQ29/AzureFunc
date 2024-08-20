206d5c5302e849e9a64d174882355b55a1fc2222
PierreQ29 <149312362+PierreQ29@users.noreply.github.com>
2024-08-20 15:08:59 +0200

    Update __init__.py

--- HttpTrigger2/__init__.py
+++ HttpTrigger2/__init__.py
@@ -1,44 +1,37 @@
 import logging
 import azure.functions as func
+import pandas as pd
 from azure.storage.blob import BlobClient
+from io import StringIO
 import os
-import tempfile
-from surprise import dump
 
-# Charger un fichier pickle depuis Azure Blob Storage
-def load_model(connection_string, container_name, file_name):
-    try:
-        logging.info("Début du chargement du modèle.")
-        blob_client = BlobClient.from_connection_string(connection_string, container_name, file_name)
-        logging.info("Client Blob créé.")
-        download_stream = blob_client.download_blob()
-        logging.info("Blob téléchargé.")
-        with tempfile.NamedTemporaryFile(delete=False) as temp_file:
-            temp_file.write(download_stream.readall())
-            temp_file_path = temp_file.name
-        logging.info("Fichier temporaire créé.")
-        _, model = dump.load(temp_file_path)
-        logging.info("Modèle chargé avec succès.")
-        return model
-    except Exception as e:
-        logging.error(f"Erreur lors du chargement du modèle: {e}")
-        return None
+# Variable globale pour stocker les IDs utilisateurs
+user_ids = []
 
-# Initialisation du modèle
+# Charger les IDs utilisateurs une seule fois lors de l'initialisation du module
+def load_user_ids(connection_string, container_name, file_name):
+    global user_ids
+    blob_client = BlobClient.from_connection_string(connection_string, container_name, file_name)
+    download_stream = blob_client.download_blob()
+    csv_content = download_stream.content_as_text()
+    df = pd.read_csv(StringIO(csv_content))
+    user_ids = df['user_id'].tolist()
+
+# Initialiser les IDs utilisateurs lors du chargement du module
 connection_string = os.getenv("AZURE_STORAGE_CONNECTION_STRING")
 container_name = "data"
-model = load_model(connection_string, container_name, "model_nmf.pickle")
+load_user_ids(connection_string, container_name, "user_id.csv")
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
-    if model is None:
-        return func.HttpResponse(
-            "Erreur lors du chargement du modèle.",
-            status_code=500
-        )
+    name = "Naaaame"
+    user_ids_string = ",".join([str(id) for id in user_ids])
 
-    return func.HttpResponse(
-        "Le modèle a été chargé avec succès.",
-        status_code=200
-    )
+    if user_ids_string:
+        return func.HttpResponse(user_ids_string, status_code=200)
+    else:
+        return func.HttpResponse(
+             "Please pass a name on the query string or in the request body",
+             status_code=400
+        )
